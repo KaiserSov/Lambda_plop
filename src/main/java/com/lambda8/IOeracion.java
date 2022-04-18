6bde3fc0b644928bfc43f7776b1570b06d3c5ead
KaiserSov <sara310105@gmail.com>
2022-04-18 15:29:58 -0500

    Lambda en operacion matemática de interface

---
+++ src/main/java/com/lambda8/IOeracion.java
@@ -0,0 +1,6 @@
+package com.lambda8;
+
+public interface IOeracion {
+
+    double calcularPromedio(double n1, double n2);
+}
--- src/main/java/com/lambda8/LambdaApp.java
+++ src/main/java/com/lambda8/LambdaApp.java
@@ -19,6 +19,7 @@ public class LambdaApp {
             }
         });*/
 
+        //Esto es Lambda
         Collections.sort(lista, (String p1, String p2) -> p1.compareTo(p2));
 
         for (String elemento : lista){
@@ -27,9 +28,32 @@ public class LambdaApp {
 
     }
 
+    public void calcular(){
+        /*IOeracion operacion = new IOeracion() {
+            @Override
+            public double calcularPromedio(double n1, double n2) {
+                return (n1 + n2) / 2;
+            }
+        };
+
+        System.out.println(operacion.calcularPromedio(2,3));*/
+
+        //Lambda
+        IOeracion operacion = (double x, double y) -> (x+y)/2;
+        operacion.calcularPromedio(2,3);
+        System.out.println(operacion.calcularPromedio(2,3));
+
+
+    }
+
     public static void main(String[] args) {
         LambdaApp app = new LambdaApp();
+        System.out.println(" ");
+        System.out.println("La lista ordenada es:");
         app.ordenar();
+        System.out.println(" ");
+        System.out.println("El promedio es:");
+        app.calcular();
 
     }
 }
