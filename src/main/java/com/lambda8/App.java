5814d1958099139a6c830d5b4261373acb979081
KaiserSov <sara310105@gmail.com>
2022-04-18 14:49:29 -0500

    Inicié una prueba de esxpresiones lambdas con una lista

---
+++ src/main/java/com/lambda8/App.java
@@ -0,0 +1,18 @@
+package com.lambda8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public void ordenar (){
+        List<String> lista = new ArrayList<>();
+        lista.add("Sara está probando lambdas");
+        lista.add("Esperemos que funcione");
+        lista.add("Brillas");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
